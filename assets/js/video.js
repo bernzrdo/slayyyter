eaedd760bde8833ba49f3d48ca0935390cdf4cec
bernardo silva <bernardovs2003@gmail.com>
2019-11-11 18:50:45 +0000

    Update video.js

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,24 +1,10 @@
 $(function(){
     
     $(".video span:nth-child(1)").click(function(){
-        $(".video").each(function(i,el){
-            $(el).children("video").hide();
-            $(el).children("video")[0].pause();
-            $(el).children("video")[0].currentTime=0;
-            $(el).children("img").show();
-            $(el).children("span").show();
-        });
-        $(this).parent().children("video").show();
-        $(this).parent().children("video")[0].play();
-        $(this).parent().children("img").hide();
-        $(this).hide();
-    });
-    
-    $("video").on("ended",function(){
-        $(this).parent().children("video")[0].currentTime=0;
-        $(this).parent().children("img").show();
-        $(this).parent().children("span").show();
-        document.exitFullscreen?document.exitFullscreen():document.mozCancelFullScreen?document.mozCancelFullScreen():document.webkitExitFullscreen?document.webkitExitFullscreen():document.msExitFullscreen&&document.msExitFullscreen();
+        $(this).parent().children("iframe").show();
+        $(this).parent().children("iframe").height($(this).parent().children("img").height());
+        $(this).parent().children("img").remove();
+        $(this).remove();
     });
     
 });
